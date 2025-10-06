6b430a5ea770cb9ee407aa02ee61c901271d7e78
Aaditya Pal <129619636+aadityapalsagwan@users.noreply.github.com>
2025-10-06 12:34:32 +0530

    Update Signup.jsx

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -20,7 +20,8 @@ export default function Signup() {
     setError('');
 
     try {
-      const response = await fetch(`${API_BASE_URL}/users/signup`, {
+      // const response = await fetch(`${API_BASE_URL}/users/signup`, {
+      const response = await fetch(`https://e-book-store-backend-my9e.onrender.com/api/users/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password }),
